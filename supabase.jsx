5d03cb854139156705dea260b78a1d5740e6ca1d
Turki <turkeksa1234@gmail.com>
2026-03-11 07:28:28 +0300

    c

--- supabase.jsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Replace these with the keys from your Supabase dashboard
-const supabaseUrl = 'https://gzzgfexnklerpffrjkid.supabase.co'
-const supabaseKey = 'sb_publishable_zcksWHYsKOKBJIjWK9CEfA_QWvEtDpm'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
